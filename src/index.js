a20d6e79c39915c7a433306880ef62823cec3ed5
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-11 23:18:54 +0900

    Display picture icon.png

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
 import './style.scss';
-import img from './icon.png';
 
  function component() {
    const element = document.createElement('div');
